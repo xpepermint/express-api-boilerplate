4402a5c2b239972b1aa8da170a73393ef0991281
xpeper <xpepermint@gmail.com>
2015-07-16 23:16:25 +0200

    optimizing sequelize initializer

--- app/models/index.js
+++ app/models/index.js
@@ -1,21 +1,8 @@
 import Sequelize from 'sequelize';
-import fs from 'fs';
+import init from 'sequelize-init'
 import config from '../../config';
 
 let sequelize = new Sequelize(config.databaseName, config.databaseUsername, config.databasePassword, {dialect: 'postgres', native: true});
-
-let db = {};
-fs.readdirSync(__dirname).filter((file) => {
-  return (file.indexOf('.') !== 0) && (file !== 'index.js');
-}).forEach((file) => {
-  if (file.slice(-3) !== '.js') return;
-  let model = sequelize.import(`${__dirname}/${file}`);
-  db[model.name] = model;
-});
-Object.keys(db).forEach((modelName) => {
-  if ('associate' in db[modelName]) db[modelName].associate(db);
-});
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+let db = init(sequelize, __dirname, {exclude: ['index.js']});
 
 export default db;
